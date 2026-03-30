08e8649956f2394a901db19dfc013a85766bf4ec
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2026-03-30 10:11:55 -0300

    chore: setup frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>PriceTracker</h1>
+        <p>Projeto iniciado com sucesso</p>
+      </div>
+    </>
+  )
+}
+
+export default App
