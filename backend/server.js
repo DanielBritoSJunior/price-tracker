7fc50d0e3fbe2e64b7fb8f2b39ce0c6c3c24138c
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2026-04-11 03:29:00 -0300

    feat: store products in memory and add get products route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const cors = require("cors")
-require ("dotenv").config()
+require("dotenv").config()
 
 const app = express()
+let products = []
 
 app.use(cors())
 app.use(express.json())
@@ -12,14 +13,20 @@ app.get("/", (req, res) => {
 })
 
 app.post("/products", (req, res) => {
-    const product = req.body
+    const product = req.body;
 
-    console.log("Produto recebido:", product)
+    products.push(product);
+
+    console.log("Produto salvo:", product);
 
     res.status(201).json({
-        message: "Produto recebido com sucesso",
+        message: "Produto salvo com sucesso",
         product,
-    })
+    });
+});
+
+app.get("/products", (req, res) => {
+    res.json(products);
 })
 
 const PORT = process.env.PORT || 3000
